4829d65f578df20c9bf1269d959e8387f54a39fc
XLazer4 <khannarahul446@gmail.com>
2022-04-22 13:29:13 +0530

    added functionality to test contracts using chai

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.5.0;
 contract Token {
     string  public name = "DApp Token";
     string  public symbol = "DAPP";
-    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens of which 18 0s are in decimals
     uint8   public decimals = 18;
 
     event Transfer(
