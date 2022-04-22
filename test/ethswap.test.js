16a0a1cc24f91123f00bf77d9301785b9441a42c
XLazer4 <khannarahul446@gmail.com>
2022-04-22 13:32:45 +0530

    test folder

---
+++ test/ethswap.test.js
@@ -0,0 +1,37 @@
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+contract('EthSwap', (accounts) => {
+
+  let token, ethSwap
+
+  before(async() => {
+    token = await Token.new()
+    ethSwap = await EthSwap.new()
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
+  })
+
+    describe('Token deployment', async () => {
+    it('contract has a name', async () => {
+      const name = await token.name()
+      assert.equal(name, 'DApp Token')
+    }) 
+  })
+
+  describe('EthSwap deployment', async () => {
+    it('contract has a name', async () => {
+      const name = await ethSwap.name()
+      assert.equal(name, 'Exchange')
+    })
+
+    it('contract has tokens', async () => {
+        let balance = await token.balanceOf(ethSwap.address)
+        assert.equal(balance.toString(),'1000000000000000000000000')
+      })
+    })
+
+})
